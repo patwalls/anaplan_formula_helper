db169150460666eb8ebd4a2e855f7986414b3b1a
Pat Walls <patrickswalls@gmail.com>
2016-05-20 11:51:11 -0700

    Added live link to download extension

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 The Anaplan Formula Helper is a Chrome Extension to make writing formulas easier in Anaplan. It is built with JavaScript and jQuery.
 
+Download it [here](https://chrome.google.com/webstore/detail/anaplan-formula-helper/ekjmcfghjgnplkmpmacbjknfbkdgbabc):
+
 ###Main Functionality:
 * View details about an open function while you're writing it.
 * Understand what parameter you're currently typing out.
--- formula.js
+++ formula.js
@@ -1,41 +1,41 @@
 $(document).arrive(".formulaBarText", function() {
 
-console.log('this formula file loads');
+  console.log('this formula file loads');
 
-var formulaInput = document.querySelector('.formulaBarText');
+  var formulaInput = document.querySelector('.formulaBarText');
 
-formulaInput.addEventListener('keyup', function(e)
-  {
-    setTimeout(function() {
-        var fullFormula = e.srcElement.value
-        var caretPosition = doGetCaretPosition(formulaInput);
-        var formulaUpToCursor = fullFormula.slice(0,caretPosition);
-        openFunc(formulaUpToCursor);
-    }, 0);
-  });
+  formulaInput.addEventListener('keyup', function(e)
+    {
+      setTimeout(function() {
+          var fullFormula = e.srcElement.value
+          var caretPosition = doGetCaretPosition(formulaInput);
+          var formulaUpToCursor = fullFormula.slice(0,caretPosition);
+          openFunc(formulaUpToCursor);
+      }, 0);
+    });
 
-var openFunc = function (formula) {
-  var closeParenCount = 0;
-  for (var i = formula.length - 1; i > -1; i--) {
-    if (formula[i] == '(') {
-      if (closeParenCount == 0) {
-        var startIdxOfLastFunc = formula.slice(0,i).search(/(\w+)$/);
-        var funcText = formula.slice(startIdxOfLastFunc,i);
-        var commas = countCommas(formula.slice(startIdxOfLastFunc, formula.length));
-        tooltip.openFunc(FUNCTIONS[funcText],commas);
-        return
-      } else {
-        closeParenCount -= 1
+  var openFunc = function (formula) {
+    var closeParenCount = 0;
+    for (var i = formula.length - 1; i > -1; i--) {
+      if (formula[i] == '(') {
+        if (closeParenCount == 0) {
+          var startIdxOfLastFunc = formula.slice(0,i).search(/(\w+)$/);
+          var funcText = formula.slice(startIdxOfLastFunc,i);
+          var commas = countCommas(formula.slice(startIdxOfLastFunc, formula.length));
+          tooltip.openFunc(FUNCTIONS[funcText],commas);
+          return
+        } else {
+          closeParenCount -= 1
+        }
+      } else if (formula[i] == ')') {
+        closeParenCount += 1;
       }
-    } else if (formula[i] == ')') {
-      closeParenCount += 1;
     }
+    tooltip.clear();
   }
-  tooltip.clear();
-}
 
-var countCommas = function(text) {
-  return text.split(',').length - 1
-}
+  var countCommas = function(text) {
+    return text.split(',').length - 1
+  }
 
 });
